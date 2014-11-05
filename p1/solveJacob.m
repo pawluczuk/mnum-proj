86cbe50dec8b7e337049a483ee1994ac66e0c0a7
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-05 10:44:19 +0100

    zad 3 - dodanie rozwiazania dla ukladu zadanego; uzupelnienie sprawozdania

--- p1/solveJacob.m
+++ p1/solveJacob.m
@@ -2,10 +2,10 @@ function [] = solveJacob( )
 % solveJacob Generuje macierze i przekazuje do rozwiazania
 % aby je rozwiazan metoda Jacobiego
 
-N = 1000;
+N = 2000;
 
 for i = 1 : 3
-    for k = 3 : 10
+    for k = 3 : 9
         [A, b] = generateMatrix(i, k);
         Jacob(A, b, N);
     end

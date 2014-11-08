03633e325d5db9e9e8eb60f1045cef9e490afee8
pawluczuk <pawluczuk.monika@gmail.com>
2014-11-08 14:35:02 +0100

    skonczony projekt pierwszy z finalnymi poprawkami; szkic projektu 2

--- p1/Jacob.m
+++ p1/Jacob.m
@@ -24,8 +24,40 @@ k = 1;
 % szukany wektor - zaczynamy od samych zer
 x = zeros(n,1);
 
+
+%warunek dostateczny zbieznosci metody jacobiego:
+for i = 1 : n
+   %dominacja wierszowa
+   disp(i);
+   dominacjaWierszowa = 1;
+   for i = 1 : n 
+    if ( abs(A(k,k)) < A(i, k) )
+        dominacjaWierszowa = 0;
+    end
+   end
+   if (dominacjaWierszowa == 1)
+       disp('Warunek dominacji wierszowej spelniony')
+   else
+       disp('Warunek dominacji wierszowej nie jest spelniony')
+   end
+   
+   %dominacja kolumnowa
+   dominacjaKolumnowa = 1;
+   for i = 1 : n 
+    if ( abs(A(k,k)) < A(k, i) )
+        dominacjaKolumnowa = 0;
+    end
+   end
+   if (dominacjaKolumnowa == 1)
+       disp('Warunek dominacji kolumnowej spelniony')
+   else
+       disp('Warunek dominacji kolumnowej nie jest spelniony')
+   end
+end
+   
 while k <= iterations
    x(:,k + 1) = Tj * x(:, k) + cj;
+   
    if norm(x(:,k + 1) - x(:, k)) < esp
       disp('Osiagnieto zadany warunek stopu: ||x^(k+1) - x^(k)|| < esp dla wartosci:')
       disp('Liczba iteracji:');
